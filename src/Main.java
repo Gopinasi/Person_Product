6e57fb6ef4294dde5bf775bd3231c286225b9125
NIO_Path_File <gopinasi@mail.uc.edu>
2024-09-08 21:32:22 -0400

    Final Commit

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,5 @@ import java.util.SortedMap;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-       /// ArrayList<Person> folks = new ArrayList<>();
-        Person Sally = new Person("Sally", "Billy", "Mrs.", 1234);
-        //Person bob = new Person("bob", "Billy", "Mrs.", 1234);
-        //System.out.println(Sally.getAge(2023));
-        System.out.println(Sally.toCSV());
-
-       // for(Person p: folks){
-         //   System.out.println(p);
         }
 }
